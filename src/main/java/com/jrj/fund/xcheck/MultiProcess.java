9b8766a9c962fa15b4c483ecb0bde6eeb33c438d
huangyan <huangyan@huangyan-PC>
2019-03-14 22:34:48 +0800

    添加获取网络资源

---
+++ src/main/java/com/jrj/fund/xcheck/MultiProcess.java
@@ -0,0 +1,131 @@
+/**
+ * cdp4j Commercial License
+ *
+ * Copyright 2017, 2019 WebFolder OÜ
+ *
+ * Permission  is hereby  granted,  to "____" obtaining  a  copy of  this software  and
+ * associated  documentation files  (the "Software"), to deal in  the Software  without
+ * restriction, including without limitation  the rights  to use, copy, modify,  merge,
+ * publish, distribute  and sublicense  of the Software,  and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  IMPLIED,
+ * INCLUDING  BUT NOT  LIMITED  TO THE  WARRANTIES  OF  MERCHANTABILITY, FITNESS  FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL  THE AUTHORS  OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.jrj.fund.xcheck;
+
+import static io.webfolder.cdp.event.Events.NetworkLoadingFinished;
+import static io.webfolder.cdp.event.Events.NetworkResponseReceived;
+import static io.webfolder.cdp.session.SessionFactory.DEFAULT_PORT;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import static java.lang.System.getProperty;
+import static java.nio.file.Paths.get;
+import static java.util.Arrays.asList;
+import io.webfolder.cdp.Launcher;
+import io.webfolder.cdp.event.network.LoadingFinished;
+import io.webfolder.cdp.event.network.ResponseReceived;
+import io.webfolder.cdp.session.Session;
+import io.webfolder.cdp.session.SessionFactory;
+import io.webfolder.cdp.type.network.GetResponseBodyResult;
+import io.webfolder.cdp.type.network.ResourceType;
+import io.webfolder.cdp.type.network.Response;
+
+public class MultiProcess {
+
+    // port number and user-data-dir must be different for each chrome process
+    // As an alternative @see IncognitoBrowsing.java for incognito mode (private browsing).
+    public static void main(String[] args) {
+
+		Set<String> finished = new HashSet<>();
+        new Thread() {
+
+            public void run() {
+                Launcher launcher = new Launcher(getFreePort(DEFAULT_PORT));
+                Path remoteProfileData = get(getProperty("java.io.tmpdir")).resolve("remote-profile-" + new Random().nextInt());
+                SessionFactory factory = launcher.launch(asList(
+//                		"--headless",
+                		"--disable-gpu",
+                		"--allow-running-insecure-content",
+                		"--user-data-dir=" + remoteProfileData.toString()));
+
+                try (SessionFactory sf = factory) {
+                    try (Session session = sf.create()) {
+            			session.getCommand().getNetwork().enable();
+            			session.addEventListener((e, d) -> {
+            				if (NetworkLoadingFinished.equals(e)) {
+            					LoadingFinished lf = (LoadingFinished) d;
+            					finished.add(lf.getRequestId());
+            				}
+            				if (NetworkResponseReceived.equals(e)) {
+            					ResponseReceived rr = (ResponseReceived) d;
+            					Response response = rr.getResponse();
+            					if(ResourceType.Script.equals(rr.getType())
+            							||ResourceType.XHR.equals(rr.getType())
+            							||ResourceType.WebSocket.equals(rr.getType())) {
+            						if(response.getUrl().startsWith("http://")) {
+            							System.out.println("URL       : " + response.getUrl());
+            						}
+            					}
+//            					System.out.println(
+//            							"Status    : HTTP " + response.getStatus().intValue() + " " + response.getStatusText());
+//            					System.out.println("Mime Type : " + response.getMimeType());
+//            					if (finished.contains(rr.getRequestId()) && ResourceType.Document.equals(rr.getType())) {
+//            						GetResponseBodyResult rb = session.getCommand().getNetwork().getResponseBody(rr.getRequestId());
+//            						if (rb != null) {
+//            							String body = rb.getBody();
+//            							// System.out.println("Content : " + body.substring(0, body.length() > 1024 ?
+//            							// 1024 : body.length()));
+//            						}
+//            					}
+            				}
+            			});
+            			session.navigate("https://fund.jrj.com.cn");
+                       // session.navigate("https://webfolder.io");
+                        session.waitDocumentReady();
+                        session.wait(3000);
+                       // System.err.println("Content Length: " + session.getContent().length());
+                    }
+                }
+              //  factory.close();
+            }
+         
+        }.start();
+
+//        new Thread() {
+//
+//            public void run() {
+//                Launcher launcher = new Launcher(getFreePort(DEFAULT_PORT));
+//                Path remoteProfileData = get(getProperty("java.io.tmpdir")).resolve("remote-profile-" + new Random().nextInt());
+//                SessionFactory factory = launcher.launch(asList("--user-data-dir=" + remoteProfileData.toString()));
+//
+//                try (SessionFactory sf = factory) {
+//                    try (Session session = sf.create()) {
+//                        session.navigate("https://webfolder.io");
+//                        session.waitDocumentReady();
+//                        System.err.println("Content Length: " + session.getContent().length());
+//                    }
+//                }
+//            }
+//        }.start();
+    }
+
+    protected static int getFreePort(int portNumber) {
+        try (ServerSocket socket = new ServerSocket(portNumber)) {
+            int freePort = socket.getLocalPort();
+            return freePort;
+        } catch (IOException e) {
+            return getFreePort(portNumber + 1);
+        }
+    }
+}
